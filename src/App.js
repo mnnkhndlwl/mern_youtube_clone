5f16b2ff4ae370d8e66a9c32d6db9f49a6d9ef4a
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-07-23 01:54:17 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { darkTheme, lightTheme } from "./utils/Theme";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
+
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div>
-      Hello world!
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+
+    
+    <Container>
+      <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Main>
+        <Navbar />
+        <Wrapper>
+
+        </Wrapper>
+      </Main>
+    </Container>
+    </ThemeProvider>
   );
 }
 
