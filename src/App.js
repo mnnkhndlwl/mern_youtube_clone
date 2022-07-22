d6eb6067d12634d3df4046c3e5ec7d06bce741cb
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-07-23 02:25:56 +0530

    router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import SignIn from "./pages/SignIn";
 
 const Container = styled.div`
   display: flex;
@@ -22,16 +26,24 @@ function App() {
   const [darkMode, setDarkMode] = useState(true);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-
-    
     <Container>
-      <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+    <BrowserRouter>
+    <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
       <Main>
         <Navbar />
         <Wrapper>
-
+        <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="signin" element={<SignIn />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
         </Wrapper>
       </Main>
+    </BrowserRouter>
     </Container>
     </ThemeProvider>
   );
