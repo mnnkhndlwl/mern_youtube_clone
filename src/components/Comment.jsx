f48ace89d1ba9146c32c1b3750606d04bfee8f2f
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-07-24 00:14:37 +0530

    new comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 30px 0px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: ${({ theme }) => theme.text}
+`;
+const Name = styled.span`
+  font-size: 13px;
+  font-weight: 500;
+`;
+
+const Date = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.textSoft};
+  margin-left: 5px;
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+`;
 
 const Comment = () => {
   return (
-    <div>Comment</div>
-  )
-}
+    <Container>
+      <Avatar src="https://qph.cf2.quoracdn.net/main-qimg-025c4b92696d130d537185aad9d7d742-lq" />
+      <Details>
+        <Name>
+          light yagami <Date>1 day ago</Date>
+        </Name>
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
+          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
+          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
+          mollitia totam reiciendis?
+        </Text>
+      </Details>
+    </Container>
+  );
+};
 
-export default Comment
+export default Comment;
