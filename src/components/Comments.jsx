536b478aea800b3c4e3dd08bae7961bf82b5bee1
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-07-24 00:09:28 +0530

    comment

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Comment from "./Comment";
+
+const Container = styled.div``;
+
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  background-color: transparent;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+`;
 
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <Container>
+    <NewComment>
+    <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCTrI1OMEutKs4-YeEoAwG37SdKrnXAc4BgA&usqp=CAU" />
+    <Input placeholder="Add a comment..." />
+  </NewComment>
+  <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+  </Container>
   )
 }
 
