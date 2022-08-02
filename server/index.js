b10cdf894410135baccd30a771ceba7675402fad
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-08-02 14:11:56 +0530

    commir

--- server/index.js
+++ server/index.js
@@ -45,11 +45,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
 
 
 app.listen(process.env.PORT || 5000, () => {
