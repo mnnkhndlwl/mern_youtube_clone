f7489e80c09d04fbbe6e4725ee64d8221bf40d9f
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-03-11 22:26:58 +0530

    fix: Mic Dark Mode Proof

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,8 +20,8 @@ const Container = styled.div`
   z-index: 100;
 
   @media (max-width: 480px) {
-  height: 48px;
-  padding: 0 20px;
+    height: 48px;
+    padding: 0 20px;
   }
 `;
 
@@ -34,18 +34,20 @@ const Wrapper = styled.div`
   position: relative;
   font-size: 5px;
 
-@media (max-width: 480px) {
-  font-size: 1px;
-  padding: 0 10px;
+  @media (max-width: 480px) {
+    font-size: 1px;
+    padding: 0 10px;
   }
 `;
 
 const Search = styled.div`
   background-color: ${({ theme }) => theme.bgLighter};
   width: 95%;
-  ${'' /* position: absolute;
+  ${
+    "" /* position: absolute;
   left: 0px;
-  right: 0px; */}
+  right: 0px; */
+  }
   margin: auto;
   display: flex;
   align-items: center;
@@ -55,9 +57,9 @@ const Search = styled.div`
   border-radius: 30px;
   color: ${({ theme }) => theme.text};
   @media (max-width: 480px) {
-  width: 50%;
-  padding: 5px;
-  margin-left: 100px;
+    width: 50%;
+    padding: 5px;
+    margin-left: 100px;
   }
 `;
 
@@ -71,8 +73,8 @@ const Input = styled.input`
   font-size: 18px;
   color: ${({ theme }) => theme.text};
   @media (max-width: 480px) {
-  padding: 5px;
-  font-size: 15px;
+    padding: 5px;
+    font-size: 15px;
   }
 `;
 
@@ -88,10 +90,10 @@ const Button = styled.button`
   align-items: center;
   gap: 5px;
   @media (max-width: 480px) {
-  margin-right:-5px ;
-  font-size: 12px;
-  padding: 2px;
-  gap: 2px;
+    margin-right: -5px;
+    font-size: 12px;
+    padding: 2px;
+    gap: 2px;
   }
 `;
 
@@ -104,8 +106,8 @@ const SearchButton = styled.div`
   padding-right: 20px;
   padding-left: 20px;
   @media (max-width: 480px) {
-  width: 50%;
-  padding: 5px;
+    width: 50%;
+    padding: 5px;
   }
 `;
 
@@ -124,8 +126,8 @@ const Avatar = styled.img`
   border-radius: 50%;
   background-color: #999;
   @media (min-width: 480px) and (max-width: 768px) {
-  width: 48px;
-  height: 48px;
+    width: 48px;
+    height: 48px;
   }
 `;
 
@@ -134,11 +136,11 @@ const SVG = styled.div`
   height: 42px;
   display: flex;
   align-items: center;
-  justify-content: center; 
+  justify-content: center;
 
   margin-left: 12px;
   border: 0.1px solid rgb(255, 255, 255, 0.05);
-  background-color: ${({theme}) => theme.bgLighter};
+  background-color: ${({ theme }) => theme.bgLighter};
   color: ${({ theme }) => theme.text};
   border-radius: 50%;
 `;
@@ -150,7 +152,7 @@ const Item = styled.div`
   }
 `;
 
-const Navbar = ({handleToggle}) => {
+const Navbar = ({ handleToggle, darkMode }) => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const [q, setQ] = useState("");
@@ -158,66 +160,81 @@ const Navbar = ({handleToggle}) => {
   const [openModal, setModel] = useState(false);
 
   const Logo = styled.div`
-  width: 150px;
-  @media (max-width: 480px) {
-  width: 48px;
-  height: 48px;
-  }
+    width: 150px;
+    @media (max-width: 480px) {
+      width: 48px;
+      height: 48px;
+    }
   `;
 
   return (
     <>
       <Container>
         <Wrapper>
-
-          <div style={{display:'flex', alignItems:'center', gap: "10px"}}>
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
             <Item>
-                <MenuIcon onClick={()=>handleToggle()}/>
+              <MenuIcon onClick={() => handleToggle()} />
             </Item>
-            <Link to="/" style={{textDecoration:'none'}}>
-              <Logo style={{display:'flex', alignItems:'center', gap:'10px'}}>
-                <img src={logo} alt="logo" style={{width: '30px'}} />
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <Logo
+                style={{ display: "flex", alignItems: "center", gap: "10px" }}
+              >
+                <img src={logo} alt="logo" style={{ width: "30px" }} />
                 <Item>
-                <h1 style={{fontSize: '25px'}}>VideoTube</h1>
+                  <h1 style={{ fontSize: "25px" }}>VideoTube</h1>
                 </Item>
               </Logo>
             </Link>
           </div>
-        
-        <div style={{
-          width: '50%',
-          display: 'flex',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}>
 
-          <Search id="search-bar">
+          <div
+            style={{
+              width: "50%",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Search id="search-bar">
+              <Input
+                placeholder="Search"
+                onChange={(e) => setQ(e.target.value)}
+                onSelect={() => {
+                  document.getElementById(
+                    "search-bar"
+                  ).style.border = `2px solid ${({ theme }) => theme.text}`;
+                  document.getElementById(
+                    "search-btn"
+                  ).style.borderLeft = `1px solid ${({ theme }) =>
+                    theme.bgLighter}`;
+                }}
+              />
 
-            <Input
-              placeholder="Search"
-              onChange={(e) => setQ(e.target.value)}
-              onSelect={() => {
-                document.getElementById('search-bar').style.border = `2px solid ${({ theme }) => theme.text}`;
-                document.getElementById('search-btn').style.borderLeft = `1px solid ${({ theme }) => theme.bgLighter}`;
+              <SearchButton id="search-btn">
+                <SearchOutlinedIcon
+                  onClick={() => navigate(`/search?q=${q}`)}
+                />
+              </SearchButton>
+            </Search>
+
+            <SVG
+              onClick={() => {
+                window.alert(`Mic feature isn't yet working ⚙️`);
               }}
-            />
-
-            <SearchButton id="search-btn">
-              <SearchOutlinedIcon onClick={() => navigate(`/search?q=${q}`)} />
-            </SearchButton>
-
-          </Search>
-
-          <SVG onClick={() => window.alert(`Mic feature isn't yet working ⚙️`)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill={({ theme }) => theme.bgLighter} class="bi bi-mic-fill" viewBox="0 0 16 16">
-              <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/>
-              <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
-            </svg>
-          </SVG>
-
-        </div>
-
-          
+            >
+              <svg
+                fill={darkMode ? "white" : ""}
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                class="bi bi-mic-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z" />
+                <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
+              </svg>
+            </SVG>
+          </div>
 
           {currentUser ? (
             <>
@@ -236,7 +253,6 @@ const Navbar = ({handleToggle}) => {
               </Button>
             </Link>
           )}
-          
         </Wrapper>
       </Container>
       {open && <Upload setOpen={setOpen} />}
